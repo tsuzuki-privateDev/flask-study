36217d677989a4860bbb9fc0f139fa73a13fd116
tadatsugu <tsuzuki2215@gmail.com>
2026-04-27 19:26:40 +0900

    feat: Jinja2テンプレートを使用した動的なデータの表示

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def index():
+    favorite_list = ["野球", "競馬", "釣り", "お笑い"]
+    return render_template("index.html", favorite_list=favorite_list)
 
 
 if __name__ == "__main__":
